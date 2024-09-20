e7caec15e8cf2a80dbf05b24b8dfa9f53afd12f6
dani <dmartinezsanz1@gmail.com>
2024-09-20 09:26:16 +0200

    cambio de nombre programa base de datos

--- basededatos.py
+++
@@ -1,8 +0,0 @@
-base_de_datos={1:{"user":"dani","contraseña":"1234"},2:{"user":"antonio","contraseña":"234"}}
-user=input("introduce un usuario")
-contraseña=input("introduce una contraseña")
-for datos in base_de_datos.values():
-    if datos["user"]==user and datos["contraseña"]==contraseña:
-        print("acceso concedido")
-
-    
