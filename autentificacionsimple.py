0e214b3d91872781c0399cfbdbc288db67e12d08
dani <dmartinezsanz1@gmail.com>
2024-09-21 04:15:53 +0200

    siguiente_version_autentificador

--- autentificacionsimple.py
+++ autentificacionsimple.py
@@ -1,8 +1,23 @@
 base_de_datos={1:{"user":"dani","contraseña":"1234"},2:{"user":"antonio","contraseña":"234"}}
-user=input("introduce un usuario")
-contraseña=input("introduce una contraseña")
-for datos in base_de_datos.values():
-    if datos["user"]==user and datos["contraseña"]==contraseña:
-        print("acceso concedido")
+acceso_concedido=False
+while not acceso_concedido:
+    usuario=input("usuario")
+    contrasenia=input("password")
+    for datos in base_de_datos.values():
+        a=datos["user"]
+        b=datos["contraseña"]
+        if a==usuario and b==contrasenia:
+            print("accceso concedido")
+            acceso_concedido=True
+    if not acceso_concedido:
+        print("usuario o clave incorrecta intentelo de nuevo")
+
 
     
+    
+        
+                
+    
+
+
+
