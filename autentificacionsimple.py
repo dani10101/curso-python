f00a87b2b29bae31866a79f4af2400abe2908a2a
dani <dmartinezsanz1@gmail.com>
2024-09-21 13:48:37 +0200

    updatealtasusersensistemadeautentificacion

--- autentificacionsimple.py
+++ autentificacionsimple.py
@@ -1,4 +1,41 @@
-base_de_datos={1:{"user":"dani","contraseña":"1234"},2:{"user":"antonio","contraseña":"234"}}
+
+base_de_datos={"1":{"user":"dani","contraseña":"1234"},"2":{"user":"antonio","contraseña":"234"}}
+#buscador de ids
+id=input("introduce un id a buscar")
+id=base_de_datos.get(id)
+print(id)
+#solicitamos un ide nuevo
+ide_nuevo=input("introduce un ide nuevo")
+usuario_nuevo=input("introduce un user nuevo")
+contra_nueva=input("introduce la contraseña para este usuario")
+#declaro otra variable con nuevos datos items del diccionario para su actualizacion
+nuevos_datos={ide_nuevo:{"user":usuario_nuevo,"contraseña":contra_nueva}}
+base_de_datos.update(nuevos_datos)
+print(base_de_datos)
+
+
+
+
+
+
+#añado id,user,contraseña al diccionario base_De_datos
+id=input("introduce un id válido")
+usuario_nuevo=input("introduce un usuario_valido")
+contrasenia=input("introduce una contraseña para este usuario")
+
+#for contrasenias in base_de_datos.get("contraseña")
+    
+
+    #lista_actualizada=lista_credenciales.insert(usuario_nuevo)
+    #id=input("indique el usuario")
+    #print(lista_actualizada)
+#continuo con el flujo del programa pidiendo un alta en el sistema
+opcion_1=input("quieres añadir un usuario a la base de datos")
+id=input("introduce un id valido a partir de 3")
+
+#for usuarios in lista_credenciales:
+#parte de login
+"""
 acceso_concedido=False
 while not acceso_concedido:
     usuario=input("usuario")
@@ -14,7 +51,7 @@ while not acceso_concedido:
 
 
     
-    
+"""
         
                 
     
