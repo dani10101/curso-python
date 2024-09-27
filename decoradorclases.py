434d209b3d6d0234d98fdf98e7774745ac79c0c1
dani <dmartinezsanz1@gmail.com>
2024-09-27 05:51:31 +0200

    decoradores

--- decoradorclases.py
+++ decoradorclases.py
@@ -1,37 +1,42 @@
+from dataclasses import dataclass, field
+@dataclass
 class Personaje:#clase padre
-    def __init__(self, nombre: str, rol: str,):
-        self.nombre = nombre
-        self.rol = rol
-        self.puntos_vida = 100
-        self.inventario = []
-        self.nivel = 1
-        self.damage=[]
-        print(f"Se ha creado a {self.nombre}, un {self.rol} de nivel {self.nivel}! con estos  {self.puntos_vida} puntos de vida")
+    nombre : str
+    rol :str
+    puntos_vida : int = 100
+    inventario :list = field(default_factory=list)
+    nivel :int = 1
+    damage: list = field(default_factory=list)
+    def __post_init__(self):
+        print(f"Se ha creado a {self.nombre}, un {self.rol} de nivel {self.nivel}! con {self.puntos_vida} puntos de vida")
+
 class Guerrero(Personaje):#clase hija
     def __init__(self, nombre,rol):
         super().__init__(nombre,rol)
         
-guts= Guerrero("Guts","guerrero")
+
 
 
 class Mago(Personaje):#clase hija
     def __init__(self, nombre,rol):
         super().__init__(nombre,rol)
         
-gandalf=Mago("Gandalf","mago")
+
 class cualidades(Personaje):#clase hija
     def __init__(self, nombre):
         super().__init__(nombre)#hereda todas las instancias o atributos del constructor de la clase padre
-    def ataque_basico(nombre):
+    def ataque_basico():
         print(gandalf.nombre,"ha atacado a ",guts.nombre)
         print(gandalf.nombre,"hace un daño de ",gandalf.damage)
+    @staticmethod
     def vida():
         lista_damage_int=[int(i) for i in gandalf.damage]
         guts.puntos_vida-=lista_damage_int[0]
         print(guts.nombre ," tiene" ,guts.puntos_vida, "de vida")
-
-        
-    
-ataque_gandalf=cualidades.ataque_basico(gandalf.damage.append(10))
-vida_guts=cualidades.vida()
-
+#creacion de instancias
+gandalf=Mago("Gandalf","mago")       
+guts= Guerrero("Guts","guerrero")
+# Ejecución de métodos
+gandalf.damage.append(10)  # Añadir daño al mago
+cualidades.ataque_basico()  # Realizar ataque básico
+cualidades.vida()  # Reducir la vida de Guts
---
+++ decoradores.py
@@ -0,0 +1,33 @@
+import pendulum
+def decorador_ruta(ruta):
+    def decorador(func):
+        def wrapper(*args, **kwargs):
+            print(f"Se ha ejecutado la función {func.__name__}")
+            resultado = func(*args, **kwargs)
+            print(f"En el path {ruta}")
+            return resultado
+        return wrapper
+    return decorador
+
+@decorador_ruta("/escritorio")
+def sumar(a, b):
+    return a + b
+
+# Al llamar sumar(10, 20), imprimirá el mensaje con su nombre, la ruta y el resultado.
+print(sumar(10, 20))
+dt = pendulum.now()
+def decorador_s(string,string_1):#funcion para decorar
+    def decorador(func):#
+        def desempaquetar_argumentos(*args,**kwargs):#desempaqueta argumentos de la funcion que usemos para ñadir con el decorador
+            print("se va a ejecutar esta funcion",func.__name__, "con lo cual puedo imprimir",string + string_1,"en este momento",dt)#aqui se dice el nombre y la hroa de lafuncion añadida
+            
+            resultado= func(*args,**kwargs)#aqui se alamcena el desmepaquetado de argumentos del afuncion concatenar
+            return resultado
+        
+        return desempaquetar_argumentos
+    return decorador
+
+@decorador_s("soy dani","hola")
+def concatenar(s,a):
+    return s + a
+print(concatenar("quetal ","adios"))    
