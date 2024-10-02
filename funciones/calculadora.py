da599e31e61fb2d2d1550ab4345980c6db9e7553
dani <dmartinezsanz1@gmail.com>
2024-10-02 23:25:04 +0200

    ultimactualizacion

---
+++ funciones/calculadora.py
@@ -0,0 +1,16 @@
+import math
+
+
+
+class calculadora():
+    def suma(num1:int,num2:int):
+        return num1 + num2
+    def resta(num1:int,num2:int):
+        return num1 - num2
+class calculadora_trig(calculadora):
+    def seno(num1:int):
+        return math.sin(num1)
+    def coseno(num1:int):
+        return math.cos(num1)
+print(calculadora_trig.suma(3,4))
+from abs import ABS
